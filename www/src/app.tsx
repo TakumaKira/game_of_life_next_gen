bf281a70e8bf9ea8fe321fa1bd62bd18b685da3a
Takuma Kira <takuma@kirakiraworx.com>
2024-04-19 13:06:12 +0900

    [dev]: Make it work in reactts-esbuild environment

--- www/src/app.tsx
+++ www/src/app.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import run from './game-of-life'
+
+const bodyStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+const fpsStyles = {
+  whiteSpace: 'pre',
+  fontFamily: 'monospace',
+}
 
 export default function App() {
-  return <h1>Hello, world</h1>;
+  const canvasRef = React.useRef()
+  React.useEffect(() => {
+    run(canvasRef.current)
+  }, [])
+  return (
+    <div style={bodyStyle}>
+      <button id="play-pause"></button>
+      <div id="fps" style={fpsStyles}></div>
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
 }
