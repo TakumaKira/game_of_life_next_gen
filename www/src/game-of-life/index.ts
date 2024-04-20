0046b9d49b17b4b2ab7d04f9f68a88ace50a1ca6
Takuma Kira <takuma@kirakiraworx.com>
2024-04-20 19:45:27 +0900

    [trial]: Show lining 10 red circles in 3D space inside almost full screen window.

--- www/src/game-of-life/index.ts
+++ www/src/game-of-life/index.ts
@@ -1,3 +1,5 @@
+import { Color3, Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, StandardMaterial, Vector3 } from 'babylonjs';
+
 import wasm from "wasm-game-of-life/wasm_game_of_life_bg.wasm";
 import * as bg from "wasm-game-of-life/wasm_game_of_life_bg.js"
 
@@ -16,8 +18,45 @@ export default async function run(canvas: HTMLCanvasElement, playPauseButton: HT
   let animationId: null | number = null
   const updateAnimId = (id: number | null) => animationId = id
   const getCurrentAnimId = () => animationId
+
+  runBabylon(canvas)
+  return { destroy: () => {} }
+
   const { onClickPlayPauseButtonFnRef, onClickNextFrameButtonFnRef, onClickCanvasFnRef } = main(canvas, wasmModule.memory, playPauseButton, nextFrameButton, fpsElement, getCurrentAnimId, updateAnimId)
   return { destroy: () => destroyImpl(onClickPlayPauseButtonFnRef, onClickNextFrameButtonFnRef, onClickCanvasFnRef, playPauseButton, nextFrameButton, canvas, getCurrentAnimId) }
+}
+
+function runBabylon(canvas: HTMLCanvasElement) {
+  canvas.height = window.innerHeight - 100;
+  canvas.width = window.innerWidth;
+  const engine = new Engine(canvas, true);
+  const scene = new Scene(engine);
+  const camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  camera.setTarget(Vector3.Zero());
+  camera.attachControl(canvas, true);
+  const light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+  light.intensity = 0.7;
+
+  const circleMesh = MeshBuilder.CreateDisc("circle", { radius: 0.5, tessellation: 50 }, scene);
+  circleMesh.rotation.x = Math.PI / 2;
+  circleMesh.registerInstancedBuffer("color", 3)
+  circleMesh.instancedBuffers.color = new Color3(1, 0, 0)
+  const circlePositions = [];
+  const numCircles = 10;
+  for (let i = 0; i < numCircles; i++) {
+    circlePositions.push(new Vector3(i, 0, 0));
+  }
+  for (let i = 0; i < circlePositions.length; i++) {
+    const newInstance = circleMesh.createInstance("circleInstance" + i);
+    newInstance.position = circlePositions[i];
+  }
+
+  engine.runRenderLoop(function () {
+    scene.render();
+  });
+  window.addEventListener("resize", function () {
+    engine.resize();
+  });
 }
 
 function main(canvas: HTMLCanvasElement, memory: WebAssembly.Memory, playPauseButton: HTMLButtonElement, nextFrameButton: HTMLButtonElement, fpsElement: HTMLDivElement, getCurrentAnimId: () => null | number, updateAnimId: (id: number | null) => void): { onClickPlayPauseButtonFnRef: () => void, onClickNextFrameButtonFnRef: () => void, onClickCanvasFnRef: (event: MouseEvent) => void } {
