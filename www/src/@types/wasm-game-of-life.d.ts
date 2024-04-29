fe2c5ed8da4a4ae7571845ef48407a42710eb898
Takuma Kira <takuma@kirakiraworx.com>
2024-04-29 21:30:35 +0900

    [refactor]: Use better naming

--- www/src/@types/wasm-game-of-life.d.ts
+++ www/src/@types/wasm-game-of-life.d.ts
@@ -2,7 +2,7 @@ type BGModule = {}
 type WASMModule = { memory: WebAssembly.Memory }
 
 declare module 'wasm-game-of-life/wasm_game_of_life_bg.wasm' {
-  export default function wasm(importObject: { './wasm_game_of_life_bg.js': BGModule }): Promise<WASMModule>
+  export default function buildWasmModule(importObject: { './wasm_game_of_life_bg.js': BGModule }): Promise<WASMModule>
 }
 
 declare module 'wasm-game-of-life/wasm_game_of_life_bg.js' {
