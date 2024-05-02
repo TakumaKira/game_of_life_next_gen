7da25a36da49d5af3aea7bd4b7b129ca07c93e86
Takuma Kira <takuma@kirakiraworx.com>
2024-05-03 03:27:29 +0900

    [refactor]: Separate setupGround function

--- www/src/game-of-life-next-gen/gl-renderer/setupGLRenderer/setupGLRenderer.ts
+++ www/src/game-of-life-next-gen/gl-renderer/setupGLRenderer/setupGLRenderer.ts
@@ -1,13 +1,12 @@
-import { ArcRotateCamera, Color3, Engine, MeshBuilder, Vector3 } from 'babylonjs'
+import { ArcRotateCamera, Color3, Engine, Vector3 } from 'babylonjs'
 import type { ICanvasRenderingContext } from 'babylonjs'
 import { EFFCT_DEFAULTS, SCENE_BACKGROUND_COLOR_DEFAULT, TEXTURE_RESOLUTION } from '@/game-of-life-next-gen/constants';
 import type { OnHoverTextureContextFn, TextContextUpdateFn } from '../types';
 import { setupGUI } from '../on-screen-controller';
 import getDefaultRenderPipeline from './getDefaultRenderPipeline';
-import getMaterialGround from './getMaterialGround';
-import getDynamicTexture from './getDynamicTexture';
 import getLight from './getLight';
 import getScene from './getScene';
+import setupGround from './setupGround';
 
 export default function setupGLRenderer(canvas: HTMLCanvasElement, onHoverTextureContext: OnHoverTextureContextFn): { updateTextureContext: (textContextUpdateFn: TextContextUpdateFn) => void, dispose: () => void} {
   const engine = new Engine(canvas, true);
@@ -23,11 +22,7 @@ export default function setupGLRenderer(canvas: HTMLCanvasElement, onHoverTextur
 
   const light = getLight("light1", new Vector3(0, 1, 0), scene, 0.7)
 
-  const groundSize = 20;
-  const ground = MeshBuilder.CreateGround("ground1", {width: groundSize, height: groundSize, subdivisions: 1}, scene);
-  const { textureGround, textureContext } = getDynamicTexture("dynamic texture", TEXTURE_RESOLUTION, scene)
-  const materialGround = getMaterialGround("Mat", scene, textureGround, new Color3(0.075, 0.075, 0.075));
-  ground.material = materialGround;
+  const { textureGround, textureContext } = setupGround(scene, 20, "ground1", "dynamic texture", "Mat", TEXTURE_RESOLUTION, new Color3(0.075, 0.075, 0.075))
 
   //Draw on canvas
   const updateTextureContext = (textContextUpdateFn: (_textureContext: ICanvasRenderingContext) => void) => {
