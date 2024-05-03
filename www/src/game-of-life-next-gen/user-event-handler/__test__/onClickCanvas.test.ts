cf54163f2206fa08bcd58eef50d7f1892d2f940c
Takuma Kira <takuma@kirakiraworx.com>
2024-05-04 00:09:20 +0900

    [test]: Fix tests manually

--- www/src/game-of-life-next-gen/user-event-handler/__test__/onClickCanvas.test.ts
+++ www/src/game-of-life-next-gen/user-event-handler/__test__/onClickCanvas.test.ts
@@ -1,4 +1,7 @@
-import onClickCanvas from './onClickCanvas';
+import onClickCanvas from '../onClickCanvas';
+import { drawGrid, drawCells } from "@/game-of-life-next-gen/drawer";
+import type { Universe } from "wasm-game-of-life/wasm_game_of_life_bg.js";
+import type { OnTextureHoverPosition, TextContextUpdateFn } from "@/game-of-life-next-gen/gl-renderer";
 
 // Mock imports
 jest.mock('@/game-of-life-next-gen/drawer', () => ({
@@ -10,21 +13,21 @@ jest.mock('@/game-of-life-next-gen/gl-renderer', () => ({
   OnTextureHoverPosition: jest.fn()
 }));
 
-jest.mock('./getRowColFromTextureHoverPosition', () => jest.fn(() => ({ row: 1, col: 1 })));
+jest.mock('../getRowColFromTextureHoverPosition', () => jest.fn(() => ({ row: 1, col: 1 })));
 
 describe('onClickCanvas', () => {
-  let universeMock, memoryMock, updateTextureContextMock, width, height, lifeSpan, onTextureHoverPosition;
+  let universeMock: Universe, memoryMock: WebAssembly.Memory, updateTextureContextMock: (textContextUpdateFn: TextContextUpdateFn) => void, width: number, height: number, lifeSpan: number, onTextureHoverPosition: OnTextureHoverPosition;
 
   beforeEach(() => {
     universeMock = {
       toggle_cell: jest.fn()
-    };
-    memoryMock = {};
+    } as unknown as Universe;
+    memoryMock = {} as WebAssembly.Memory;
     updateTextureContextMock = jest.fn();
     width = 10;
     height = 10;
     lifeSpan = 100;
-    onTextureHoverPosition = { x: 5, y: 5 };
+    onTextureHoverPosition = { x: 5, z: 5 };
   });
 
   afterEach(() => {
