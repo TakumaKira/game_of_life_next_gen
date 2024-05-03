7416ec4a6d7ec4d20cf6c404f20f23a4a23ebb9d
Takuma Kira <takuma@kirakiraworx.com>
2024-05-04 03:04:40 +0900

    [test]: Improve tests manually

--- www/src/game-of-life-next-gen/__test__/setup.test.ts
+++ www/src/game-of-life-next-gen/__test__/setup.test.ts
@@ -1,5 +1,10 @@
 // Import the function to test
+import { FPS, getUniverse } from '../game-of-life';
+import { setupGLRenderer } from '../gl-renderer';
 import setup from '../setup';
+import { onClickCanvas, onClickNextFrameButton, onClickPlayPauseButton } from '../user-event-handler';
+
+const mockDisposeGLRenderer = jest.fn();
 
 // Mock dependencies as needed
 jest.mock('@/game-of-life-next-gen/game-of-life', () => ({
@@ -12,11 +17,12 @@ jest.mock('@/game-of-life-next-gen/user-event-handler', () => ({
   onClickPlayPauseButton: jest.fn().mockReturnValue({ isPlaying: false }),
 }));
 jest.mock('@/game-of-life-next-gen/gl-renderer', () => ({
-  setupGLRenderer: jest.fn(() => ({ updateTextureContext: jest.fn(), dispose: jest.fn() })),
+  setupGLRenderer: jest.fn(() => ({ updateTextureContext: jest.fn(), dispose: mockDisposeGLRenderer })),
 }));
 
 describe('setup function', () => {
   let canvas: HTMLCanvasElement | null;
+  let mockCanvasAddEventListener: jest.SpyInstance | null;
   let updatePlayingState: jest.Mock | null;
   let updateFpsData: jest.Mock | null;
   let memory: WebAssembly.Memory | null;
@@ -25,6 +31,7 @@ describe('setup function', () => {
 
   beforeEach(() => {
     canvas = document.createElement('canvas');
+    mockCanvasAddEventListener = jest.spyOn(canvas, 'addEventListener');
     updatePlayingState = jest.fn();
     updateFpsData = jest.fn();
     memory = new WebAssembly.Memory({ initial: 1 });
@@ -35,6 +42,7 @@ describe('setup function', () => {
   afterEach(() => {
     // Clean up after each test
     canvas = null;
+    mockCanvasAddEventListener = null;
     updatePlayingState = null;
     updateFpsData = null;
     memory = null;
@@ -44,6 +52,11 @@ describe('setup function', () => {
 
   test('returns correct functions and state', () => {
     const result = setup(canvas!, updatePlayingState!, updateFpsData!, memory!, getCurrentAnimId!, updateAnimId!);
+
+    expect(getUniverse).toHaveBeenCalled();
+    expect(setupGLRenderer).toHaveBeenCalled();
+    expect(FPS).toHaveBeenCalledWith(updateFpsData);
+    expect(mockCanvasAddEventListener).toHaveBeenCalledWith('click', expect.any(Function));
 
     // Ensure the correct functions are returned
     expect(typeof result.onTogglePlayPause).toBe('function');
@@ -56,13 +69,25 @@ describe('setup function', () => {
     result.onTogglePlayPause();
 
     // Ensure updatePlayingState was called with correct value
-    expect(updatePlayingState).toHaveBeenCalledWith(expect.any(Boolean));
+    expect(onClickPlayPauseButton).toHaveBeenCalledWith({ updateFpsData: expect.any(Function) }, expect.any(Object), memory, expect.any(Function), 0, 0, 0, getCurrentAnimId, updateAnimId);
+    expect(updatePlayingState).toHaveBeenCalledWith(false);
+
+    expect(result.getIsPlaying()).toBe(false);
 
     // Simulate calling the next frame function
     result.onNextFrame();
 
+    expect(onClickNextFrameButton).toHaveBeenCalledWith(expect.any(Object), memory, expect.any(Function), 0, 0, 0);
+
     // Simulate clicking on canvas
     result.onClickCanvasFnRef();
+
+    expect(onClickCanvas).toHaveBeenCalledWith(expect.any(Object), memory, expect.any(Function), 0, 0, 0, null);
+
+    result.dispose();
+
+    expect(mockDisposeGLRenderer).toHaveBeenCalled();
+
     // You can add more assertions here if necessary
   });
 
