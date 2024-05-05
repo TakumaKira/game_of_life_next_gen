5d5f21c026ea3080c89f0768fea47cde8a501652
Takuma Kira <takuma@kirakiraworx.com>
2024-05-05 18:47:11 +0900

    [test]: Fix broken tests

--- www/src/game-of-life-next-gen/getInterface/__test__/playImpl.test.ts
+++ www/src/game-of-life-next-gen/getInterface/__test__/playImpl.test.ts
@@ -1,9 +1,9 @@
+import type { AnimationState } from "@/game-of-life-next-gen/anim-controller";
 import playImpl from "../playImpl";
 
 describe("playImpl function", () => {
   // Mock functions
   const onTogglePlayPause = jest.fn();
-  const getIsPlaying = jest.fn();
   
   beforeEach(() => {
     jest.clearAllMocks();
@@ -11,26 +11,27 @@ describe("playImpl function", () => {
 
   it("should call onTogglePlayPause if not playing and not destroyed", () => {
     const destroyedState = { isDestroyed: false };
-    getIsPlaying.mockReturnValue(false);
+    const animationState = { isPlaying: false};
 
-    playImpl(onTogglePlayPause, getIsPlaying, destroyedState);
+    playImpl(onTogglePlayPause, animationState as AnimationState, destroyedState);
 
     expect(onTogglePlayPause).toHaveBeenCalled();
   });
 
   it("should not call onTogglePlayPause if playing", () => {
     const destroyedState = { isDestroyed: false };
-    getIsPlaying.mockReturnValue(true);
+    const animationState = { isPlaying: true};
 
-    playImpl(onTogglePlayPause, getIsPlaying, destroyedState);
+    playImpl(onTogglePlayPause, animationState as AnimationState, destroyedState);
 
     expect(onTogglePlayPause).not.toHaveBeenCalled();
   });
 
   it("should not call onTogglePlayPause if destroyed", () => {
     const destroyedState = { isDestroyed: true };
+    const animationState = { isPlaying: false};
 
-    playImpl(onTogglePlayPause, getIsPlaying, destroyedState);
+    playImpl(onTogglePlayPause, animationState as AnimationState, destroyedState);
 
     expect(onTogglePlayPause).not.toHaveBeenCalled();
   });
