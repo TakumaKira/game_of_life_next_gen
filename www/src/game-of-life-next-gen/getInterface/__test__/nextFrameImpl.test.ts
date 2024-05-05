b21677ec119a426298e7f98aa591912c1e8432e2
Takuma Kira <takuma@kirakiraworx.com>
2024-05-05 18:38:50 +0900

    [test]: Fix broken tests

--- www/src/game-of-life-next-gen/getInterface/__test__/nextFrameImpl.test.ts
+++ www/src/game-of-life-next-gen/getInterface/__test__/nextFrameImpl.test.ts
@@ -1,10 +1,13 @@
+import type { AnimationState } from '@/game-of-life-next-gen/anim-controller';
 import nextFrameImpl from '../nextFrameImpl';
 import type { DestroyedState } from '../types';
 
 // Mock functions
 const onNextFrameMock = jest.fn();
-const pauseMock = jest.fn();
-const getIsPlayingMock = jest.fn();
+const animationState = {
+  isPlaying: false,
+  cancel: jest.fn(),
+};
 
 describe('nextFrameImpl', () => {
   afterEach(() => {
@@ -13,31 +16,33 @@ describe('nextFrameImpl', () => {
 
   it('calls onNextFrame() when destroyedState.isDestroyed is false', () => {
     const destroyedState: DestroyedState = { isDestroyed: false };
-    nextFrameImpl(onNextFrameMock, pauseMock, getIsPlayingMock, destroyedState);
+    animationState.isPlaying = true;
+    nextFrameImpl(onNextFrameMock, animationState as unknown as AnimationState, destroyedState);
+    expect(animationState.cancel).toHaveBeenCalled();
     expect(onNextFrameMock).toHaveBeenCalled();
-    expect(pauseMock).not.toHaveBeenCalled();
   });
 
   it('does not call onNextFrame() when destroyedState.isDestroyed is true', () => {
     const destroyedState: DestroyedState = { isDestroyed: true };
-    nextFrameImpl(onNextFrameMock, pauseMock, getIsPlayingMock, destroyedState);
+    animationState.isPlaying = true;
+    nextFrameImpl(onNextFrameMock, animationState as unknown as AnimationState, destroyedState);
+    expect(animationState.cancel).not.toHaveBeenCalled();
     expect(onNextFrameMock).not.toHaveBeenCalled();
-    expect(pauseMock).not.toHaveBeenCalled();
   });
 
-  it('calls pause() when getIsPlaying() returns true', () => {
+  it('calls cancel() when AnimationState.isPlaying returns true', () => {
+    animationState.isPlaying = true;
     const destroyedState: DestroyedState = { isDestroyed: false };
-    getIsPlayingMock.mockReturnValueOnce(true);
-    nextFrameImpl(onNextFrameMock, pauseMock, getIsPlayingMock, destroyedState);
-    expect(pauseMock).toHaveBeenCalled();
+    nextFrameImpl(onNextFrameMock, animationState as unknown as AnimationState, destroyedState);
+    expect(animationState.cancel).toHaveBeenCalled();
     expect(onNextFrameMock).toHaveBeenCalled();
   });
 
-  it('does not call pause() when getIsPlaying() returns false', () => {
+  it('does not call cancel() when AnimationState.isPlaying returns false', () => {
+    animationState.isPlaying = false;
     const destroyedState: DestroyedState = { isDestroyed: false };
-    getIsPlayingMock.mockReturnValueOnce(false);
-    nextFrameImpl(onNextFrameMock, pauseMock, getIsPlayingMock, destroyedState);
-    expect(pauseMock).not.toHaveBeenCalled();
+    nextFrameImpl(onNextFrameMock, animationState as unknown as AnimationState, destroyedState);
+    expect(animationState.cancel).not.toHaveBeenCalled();
     expect(onNextFrameMock).toHaveBeenCalled();
   });
 });
