8478e9193e8e5d22ea5c06114777203d884c83f5
Takuma Kira <takuma@kirakiraworx.com>
2024-05-05 18:27:28 +0900

    [test]: Fix broken tests

--- www/src/game-of-life-next-gen/getInterface/__test__/destroyImpl.test.ts
+++ www/src/game-of-life-next-gen/getInterface/__test__/destroyImpl.test.ts
@@ -2,32 +2,23 @@ import destroyImpl from '../destroyImpl';
 import type { DestroyedState } from '../types';
 
 describe('destroyImpl function', () => {
-  let updatePlayingState: jest.Mock<void, [boolean]>;
   let onDestroy: jest.Mock<{ isDestroyed: boolean }, []>;
   let destroyedState: DestroyedState;
 
   beforeEach(() => {
-    updatePlayingState = jest.fn();
     onDestroy = jest.fn().mockReturnValue({ isDestroyed: true });
     destroyedState = { isDestroyed: false };
   });
 
-  it('should call updatePlayingState with false', () => {
-    destroyImpl(onDestroy, updatePlayingState, destroyedState);
+  it('should call onDestroy', () => {
+    destroyImpl(onDestroy, destroyedState);
 
-    expect(updatePlayingState).toHaveBeenCalledWith(false);
+    expect(onDestroy).toHaveBeenCalled();
   });
 
   it('should update destroyedState based on onDestroy result', () => {
-    destroyImpl(onDestroy, updatePlayingState, destroyedState);
+    destroyImpl(onDestroy, destroyedState);
 
     expect(destroyedState.isDestroyed).toBe(true);
-  });
-
-  it('should call onDestroy and updatePlayingState with false', () => {
-    destroyImpl(onDestroy, updatePlayingState, destroyedState);
-
-    expect(onDestroy).toHaveBeenCalled();
-    expect(updatePlayingState).toHaveBeenCalledWith(false);
   });
 });
