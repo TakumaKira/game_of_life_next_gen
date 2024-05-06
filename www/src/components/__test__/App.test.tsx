315fd97848e9dbce51d8e343e418689a2f57c0a8
Takuma Kira <takuma@kirakiraworx.com>
2024-05-07 01:10:09 +0900

    [test]: Fix mock manually

--- www/src/components/__test__/App.test.tsx
+++ www/src/components/__test__/App.test.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import App from './App';
+import '@testing-library/jest-dom'
+import App from '../App';
 
 // Mocking getInterface and getController
 jest.mock('@/game-of-life-next-gen', () => ({
   getInterface: jest.fn(),
 }));
 jest.mock('@/hooks', () => ({
-  getController: jest.fn(),
+  getController: jest.fn().mockImplementation((getInterface, canvasRef, updatePlayingState, updateFpsData) => ({
+    play: jest.fn().mockImplementation(() => updatePlayingState(true)),
+    pause: jest.fn().mockImplementation(() => updatePlayingState(false)),
+    nextFrame: jest.fn(),
+    destroy: jest.fn(),
+  })),
 }));
 
 describe('App component', () => {
