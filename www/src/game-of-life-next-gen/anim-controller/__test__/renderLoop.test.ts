c87f7a0f2963bc3d2c14255619df5ad38b381cfd
Takuma Kira <takuma@kirakiraworx.com>
2024-05-18 16:01:32 +0900

    [test]: Fix tests for renderLoop

--- www/src/game-of-life-next-gen/anim-controller/__test__/renderLoop.test.ts
+++ www/src/game-of-life-next-gen/anim-controller/__test__/renderLoop.test.ts
@@ -1,30 +1,18 @@
 import renderLoop from '../renderLoop'; // Import the function to test
-import { drawGrid, drawCells } from "@/game-of-life-next-gen/drawer"; // Import functions used in renderLoop
 import type { Universe } from "wasm-game-of-life/wasm_game_of_life_bg.js"; // Import Universe type
 // Import other necessary dependencies and types
 import type { FPS } from "@/game-of-life-next-gen/game-of-life";
-import type { TextContextUpdateFn } from "@/game-of-life-next-gen/gl-renderer";
 import type AnimationState from '../AnimationState';
 
-// Mock dependencies
-jest.mock('@/game-of-life-next-gen/drawer', () => ({
-  drawGrid: jest.fn(),
-  drawCells: jest.fn()
-}));
-
 describe('renderLoop', () => {
-  let fps: FPS, universe: Universe, memory: WebAssembly.Memory, updateTextureContext: (textContextUpdateFn: TextContextUpdateFn) => void, width: number, height: number, lifeSpan: number, updateAnimId: (id: number | null) => void;
+  let fps: FPS, universe: Universe, updateUniverse: () => void, speed: number;
 
   beforeEach(() => {
     // Initialize variables or mocks before each test
     fps = { render: jest.fn() } as unknown as FPS;
     universe = { tick: jest.fn() } as unknown as Universe;
-    memory = new WebAssembly.Memory({ initial: 10 });
-    updateTextureContext = jest.fn();
-    width = 10;
-    height = 10;
-    lifeSpan = 100;
-    updateAnimId = jest.fn();
+    updateUniverse = jest.fn();
+    speed = 5;
   });
 
   afterEach(() => {
@@ -44,17 +32,16 @@ describe('renderLoop', () => {
       requestNextFrame: jest.fn()
     } as unknown as AnimationState;
 
-    renderLoop(fps, universe, memory, updateTextureContext, width, height, lifeSpan, mockAnimationState);
+    renderLoop(fps, universe, updateUniverse, mockAnimationState, speed);
 
     // FPS should be rendered
     expect(fps.render).toHaveBeenCalled();
 
     // Grid and cells should be drawn
-    expect(drawGrid).toHaveBeenCalledWith(updateTextureContext, width, height);
-    expect(drawCells).toHaveBeenCalledWith(universe, memory, updateTextureContext, width, height, lifeSpan);
+    expect(updateUniverse).toHaveBeenCalled();
 
     // Expect `universe.tick` to be called 9 times with `false` as the argument each time
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i < speed; i++) {
       expect(universe.tick).toHaveBeenNthCalledWith(i + 1, false);
     }
 
