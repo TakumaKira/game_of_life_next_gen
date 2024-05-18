e33fb72abe97c960fea058110114aa72d4b47b4d
Takuma Kira <takuma@kirakiraworx.com>
2024-05-18 17:30:01 +0900

    [test]: Modify tests for updated getRowColFromTextureHoverPosition

--- www/src/game-of-life-next-gen/user-event-handler/__test__/getRowColFromTextureHoverPosition.test.ts
+++ www/src/game-of-life-next-gen/user-event-handler/__test__/getRowColFromTextureHoverPosition.test.ts
@@ -1,28 +1,31 @@
-import { DEFAULT_FIELD_SIZE, TEXTURE_SIZE } from '@/game-of-life-next-gen/constants';
+import { TEXTURE_SIZE } from '@/game-of-life-next-gen/constants';
 import getRowColFromTextureHoverPosition from '../getRowColFromTextureHoverPosition';
 
 describe('getRowColFromTextureHoverPosition', () => {
   it('should return correct row and column for the center of texture', () => {
+    const fieldSize = 8
     const centerOfTexture = { x: 0, z: 0 };
-    const result = getRowColFromTextureHoverPosition(centerOfTexture);
-    expect(result.row).toEqual(Math.floor(0.5 * DEFAULT_FIELD_SIZE));
-    expect(result.col).toEqual(Math.floor(0.5 * DEFAULT_FIELD_SIZE));
+    const result = getRowColFromTextureHoverPosition(centerOfTexture, fieldSize);
+    expect(result.row).toEqual(Math.floor(0.5 * fieldSize));
+    expect(result.col).toEqual(Math.floor(0.5 * fieldSize));
   });
 
   it('should return correct row and column for the first cell position', () => {
-    const halfSizeOfCell = TEXTURE_SIZE / DEFAULT_FIELD_SIZE / 2;
+    const fieldSize = 8
+    const halfSizeOfCell = TEXTURE_SIZE / fieldSize / 2;
     const firstCellPosition = { x: -(TEXTURE_SIZE / 2 - halfSizeOfCell), z: TEXTURE_SIZE / 2 - halfSizeOfCell };
-    const result = getRowColFromTextureHoverPosition(firstCellPosition);
+    const result = getRowColFromTextureHoverPosition(firstCellPosition, fieldSize);
     expect(result.row).toEqual(0);
     expect(result.col).toEqual(0);
   });
 
   it('should return correct row and column for the last cell position', () => {
-    const halfSizeOfCell = TEXTURE_SIZE / DEFAULT_FIELD_SIZE / 2;
+    const fieldSize = 8
+    const halfSizeOfCell = TEXTURE_SIZE / fieldSize / 2;
     const lastCellPosition = { x: TEXTURE_SIZE / 2 - halfSizeOfCell, z: -(TEXTURE_SIZE / 2 - halfSizeOfCell) };
-    const result = getRowColFromTextureHoverPosition(lastCellPosition);
-    expect(result.row).toEqual(Math.floor(DEFAULT_FIELD_SIZE - 1));
-    expect(result.col).toEqual(Math.floor(DEFAULT_FIELD_SIZE - 1));
+    const result = getRowColFromTextureHoverPosition(lastCellPosition, fieldSize);
+    expect(result.row).toEqual(Math.floor(fieldSize - 1));
+    expect(result.col).toEqual(Math.floor(fieldSize - 1));
   });
 
   // Add more test cases for edge cases and other scenarios as needed
