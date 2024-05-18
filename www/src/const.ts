82510593c33ba20077e5e7c0ae9c4fd51fd4cf7b
Takuma Kira <takuma@kirakiraworx.com>
2024-05-18 15:38:08 +0900

    [refactor]: Show warning if invalid ALIVE_CELL_BASE_OPTIONS is set

--- www/src/const.ts
+++ www/src/const.ts
@@ -5,9 +5,11 @@ const ALIVE_CELL_BASE_OPTIONS_ENV = (() => {
   }
   const _ALIVE_CELL_BASE_OPTIONS_ENV = parseInt(process.env.ALIVE_CELL_BASE_OPTIONS)
   if (isNaN(_ALIVE_CELL_BASE_OPTIONS_ENV)) {
+    console.warn(`Configured ALIVE_CELL_BASE_OPTIONS is not a number: ${process.env.ALIVE_CELL_BASE_OPTIONS}`)
     return null
   }
   if (_ALIVE_CELL_BASE_OPTIONS_ENV < 0) {
+    console.warn(`Configured ALIVE_CELL_BASE_OPTIONS is a negative number: ${process.env.ALIVE_CELL_BASE_OPTIONS}`)
     return null
   }
   return _ALIVE_CELL_BASE_OPTIONS_ENV
