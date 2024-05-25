92e04a4eb976581c21d01b2a6af2c276a6cfb869
Takuma Kira <takuma@kirakiraworx.com>
2024-05-26 00:30:03 +0900

    [dev]: Prepare EffectsPanel wireframe

--- www/src/components/ui/OptionController/Panels/EffectsPanel.tsx
+++ www/src/components/ui/OptionController/Panels/EffectsPanel.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import ColorsSetting from './ColorsSetting';
+import EffectsSetting from './EffectsSetting';
+import Scrollable from './Scrollable';
 
 export const TITLE = 'Colors and Effects';
 
@@ -7,11 +10,21 @@ export const WIDTH = 600;
 
 const Container = styled.div`
 `;
+const Button = styled.button`
+`
+const HR = styled.hr`
+`
 
 export default function EffectsPanel() {
   return (
     <Container>
-      Effects
+      <Scrollable>
+        <ColorsSetting />
+        <Button>Reset Colors</Button>
+        <HR />
+        <EffectsSetting />
+        <Button>Reset Effects</Button>
+      </Scrollable>
     </Container>
   )
 }
