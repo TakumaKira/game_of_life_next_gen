2872d21f9260d98faaa12e39580b6ad40eadcd2c
Takuma Kira <takuma@kirakiraworx.com>
2024-06-07 13:41:14 +0900

    [fix]: Avoid process is not defined reference error if .env file was not provided

--- www/src/const.ts
+++ www/src/const.ts
@@ -1,5 +1,6 @@
 const ALIVE_CELL_BASE_OPTIONS_DEFAULT = 10
-const ALIVE_CELL_BASE_OPTIONS_ENV = process.env.ALIVE_CELL_BASE_OPTIONS
+const PROCESS_ENV = typeof process !== "undefined" && process ? process.env : {}
+const ALIVE_CELL_BASE_OPTIONS_ENV = PROCESS_ENV.ALIVE_CELL_BASE_OPTIONS
 export const ALIVE_CELL_BASE_OPTIONS = sanitizeAliveCellBaseOptionsEnv(ALIVE_CELL_BASE_OPTIONS_ENV) ?? ALIVE_CELL_BASE_OPTIONS_DEFAULT
 
 /** Exported for TESTING PURPOSE ONLY and this function should not be moved to another file to prevent circular reference */
@@ -9,11 +10,11 @@ export function sanitizeAliveCellBaseOptionsEnv(fromEnv: string | undefined): nu
   }
   const _ALIVE_CELL_BASE_OPTIONS_ENV = parseInt(fromEnv)
   if (isNaN(_ALIVE_CELL_BASE_OPTIONS_ENV)) {
-    console.warn(`Configured ALIVE_CELL_BASE_OPTIONS is not a number: ${process.env.ALIVE_CELL_BASE_OPTIONS}`)
+    console.warn(`Configured ALIVE_CELL_BASE_OPTIONS is not a number: ${PROCESS_ENV.ALIVE_CELL_BASE_OPTIONS}`)
     return null
   }
   if (_ALIVE_CELL_BASE_OPTIONS_ENV < 0) {
-    console.warn(`Configured ALIVE_CELL_BASE_OPTIONS is a negative number: ${process.env.ALIVE_CELL_BASE_OPTIONS}`)
+    console.warn(`Configured ALIVE_CELL_BASE_OPTIONS is a negative number: ${PROCESS_ENV.ALIVE_CELL_BASE_OPTIONS}`)
     return null
   }
   return _ALIVE_CELL_BASE_OPTIONS_ENV
